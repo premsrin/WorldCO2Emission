5feb882194dc5477c9bc95bd3fec363c7870523e
premsrin <116606008+premsrin@users.noreply.github.com>
2022-12-05 13:26:21 -0500

    Add files via upload

---
+++ pages/What Can We Do.py
@@ -0,0 +1,36 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import plotly.express as px
+import streamlit as st
+import altair as alt
+import seaborn as sns # visualization
+from io import BytesIO
+import numpy as np
+
+st.markdown("# What can we do")
+
+st.write("It will take effort by all of us to achieve a future with clean energy and clean transportation. Some of the policies, technologies, and actions that can help are described below. As we move to a more sustainable society, we’ll need big thinkers, inventors, researchers, and policymakers to develop solutions")
+
+st.write("Our first step is to reduce the greenhouse gas emissions responsible for climate change. We need to use more clean energy – and use that energy more efficiently — to power everything from our cell phones to our homes, offices, and factories. We need to drive cars and trucks that use less gasoline or run on electricity or other alternative fuels. Everyone can play a part, including students and teachers.")
+
+st.subheader("Electricity")
+st.write("Twenty-nine percent of U.S. greenhouse gas emissions come from generating electricity. Most of the electricity is used in our homes, offices, and factories to power everything from heating and cooling systems to lights, computers, refrigerators, and cell phones.")
+
+st.subheader('Buildings')
+st.write("Greenhouse gas emissions from buildings result primarily from powering lighting fixtures, appliances, and heating and cooling systems.")
+
+st.subheader('Transportation')
+st.write("Transportation is the largest source of greenhouse gas emissions in the United States, primarily due to cars and trucks that burn gasoline and diesel fuel. Some ways to reduce these emissions include:")
+
+st.subheader('Industry')
+st.write("About a fifth of U.S. greenhouse gas emissions come from industry, including from making iron and steel, cement, and aluminum.")
+
+st.subheader('Agriculture')
+st.write("Agriculture contributes 9 percent of total U.S. greenhouse gas emissions. Emissions come from crop agriculture, such as rice or corn, and from livestock. We can reduce emissions by changing how we grow and manage crops and livestock.")
+
+st.subheader('Source')
+st.write("https://www.c2es.org/content/what-we-can-do/#:~:text=Greenhouse%20gas%20emissions%20can%20be,renewable%20hydrogen%2C%20and%20geothermal%20energy")
+
+
+
+
